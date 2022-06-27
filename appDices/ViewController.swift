b9159e1cdabddb20629f0673be96604d63200568
German Puentes <germanpuentesappdev@gmail.com>
2022-06-26 19:03:31 -0500

    app que lanza los dados al pulsa el botón

--- appDices/ViewController.swift
+++ appDices/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgDadoUno: UIImageView!
+    @IBOutlet weak var imgDadoDos: UIImageView!
+    let dados = [
+        UIImage(named: "dice1"),
+        UIImage(named: "dice2"),
+        UIImage(named: "dice3"),
+        UIImage(named: "dice4"),
+        UIImage(named: "dice5"),
+        UIImage(named: "dice6"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnLanzarDados(_ sender: UIButton) {
+        imgDadoUno.image = dados[Int.random(in: 0 ... 5)]
+        imgDadoDos.image = dados[Int.random(in: 0 ... 5)]
+    }
 }
 
